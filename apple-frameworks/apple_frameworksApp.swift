34c14e1fcfe90e7e6ed8e54d8eaa4e6df465a643
zacmonroe <zmonroe0820@gmail.com>
2023-11-28 07:48:45 -0600

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by Zac Milano on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
