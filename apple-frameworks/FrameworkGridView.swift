4aa8b622450e050ea6439583cb6f52871366773f
zacmonroe <zmonroe0820@gmail.com>
2023-11-28 17:58:21 -0600

    refactor: move stackview back into main

--- apple-frameworks/FrameworkGridView.swift
+++ apple-frameworks/FrameworkGridView.swift
@@ -28,6 +28,26 @@ struct FrameworkGridView: View {
     }
 }
 
+struct FrameworkStackView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90)
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
